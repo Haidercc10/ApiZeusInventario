2116d25cb8008dc40c23e63a326c1a9cc82a4972
Diego Fernandez <diegofe21605@gmail.com>
2023-01-12 11:11:54 -0500

    Actualización

--- ZeusInventarioWebAPI/Controllers/MovimientoItemController .cs
+++ ZeusInventarioWebAPI/Controllers/MovimientoItemController .cs
@@ -64,20 +64,19 @@ namespace ZeusInventarioWebAPI.Controllers
 
 
         // GET: api/MovimientoItems/5
-        [HttpGet("FacturacionTodosMeses/{mes}")]
-        public ActionResult FacturacionTodosMeses(int mes)
+        [HttpGet("FacturacionTodosMeses/{mes}/{ano}")]
+        public ActionResult FacturacionTodosMeses(int mes, int ano)
         {
             if (_context.MovimientoItems == null)
             {
                 return NotFound();
             }
-            var ano = DateTime.Today;
 
             var MovimientoItem = (from mi in _context.Set<MovimientoItem>()
                                         where mi.Fuente == "FV"
                                         && mi.Estado == "Procesado"
                                         && mi.FechaDocumento.Month == mes
-                                        && mi.FechaDocumento.Year == ano.Year
+                                        && mi.FechaDocumento.Year == ano
                                         select mi.PrecioTotal).Sum();
 
             var Transaccion1 = (from tr in _context.Set<Transac>()
@@ -85,7 +84,7 @@ namespace ZeusInventarioWebAPI.Controllers
                                         && tr.Tipofac == "FA"
                                         && tr.Indcpitra == "1"
                                         && tr.Fgratra.Month == mes
-                                        && tr.Fgratra.Year == ano.Year
+                                        && tr.Fgratra.Year == ano
                                         select tr.Valortra).Sum();
             var datos = MovimientoItem - Transaccion1;
 
@@ -212,15 +211,8 @@ namespace ZeusInventarioWebAPI.Controllers
             var con = from mov in _context.Set<MovimientoItem>()
                       from cli in _context.Set<Cliente>()
                       from ped in _context.Set<PedidoDeCliente>()
-                      from art in _context.Set<Articulo>()
-                          //from ext in _context.Set<Existencia>()
-                      join ext in _context.Set<Existencia>() on art.IdArticulo equals ext.Articulo
                       where mov.TipoDocumento == 7
                             && cli.Idcliente == mov.Tercero
-                            //&& ext.Articulo == art.IdArticulo
-                            && art.Codigo == mov.CodigoArticulo
-                            && art.DesHabilitado == false
-                            && art.Presentacion == mov.Presentacion
                             && ped.Consecutivo == mov.Consecutivo
                             && mov.Consecutivo == consecutivo
                       select new
@@ -232,7 +224,13 @@ namespace ZeusInventarioWebAPI.Controllers
                           cli.Ciudad,
                           Id_Producto = mov.CodigoArticulo,
                           Producto = mov.NombreArticulo,
-                          ext.Existencias,
+                          Existencias = (from art in _context.Set<Articulo>()
+                                         join ext in _context.Set<Existencia>() on art.IdArticulo equals ext.Articulo
+                                         where art.Codigo == mov.CodigoArticulo
+                                               && art.DesHabilitado == false
+                                               && art.Presentacion == mov.Presentacion
+                                         group ext by ext.Articulo into ext
+                                         select ext.Sum(x => x.Existencias)).FirstOrDefault(),
                           Cant_Pedida = mov.Cantidad,
                           Cant_Pendiente = (mov.Cantidad - mov.Faltantes),
                           Cant_Facturada = mov.Faltantes,
@@ -248,9 +246,164 @@ namespace ZeusInventarioWebAPI.Controllers
                           Empresa = "Plasticaribe SAS",
                           NIT = 800188732,
                           Direccion = "Calle 42 #52-105",
-                          Ciudad_Empresa = "Barranquilla"
+                          Ciudad_Empresa = "Barranquilla"                         
+                      };
+            return Ok(con);
+        }
 
-                         
+        //GET: Consulta para obtener el listado de clientes
+        [HttpGet("getPedidosCliente")]
+        public ActionResult GetPedidosClientes()
+        {
+            var con = from mov in _context.Set<MovimientoItem>()
+                      from cli in _context.Set<Cliente>()
+                      from ped in _context.Set<PedidoDeCliente>()
+                      where mov.TipoDocumento == 7
+                            && mov.Estado != "Liquidado"
+                            && mov.Faltantes < mov.Cantidad
+                            && cli.Idcliente == mov.Tercero
+                            && ped.Consecutivo == mov.Consecutivo
+                      group mov by new
+                      {
+                          Id_Cliente = mov.Tercero,
+                          Cliente = mov.NombreTercero
+                      } into mov
+                      select new
+                      {
+                          mov.Key.Id_Cliente,
+                          mov.Key.Cliente,
+                          Costo = (mov.Sum(x => x.Cantidad * x.PrecioUnidad)),
+                          Cantidad = mov.Count()
+                      };
+            return Ok(con);
+        }
+
+        //GET: Consulta para obtener el listado de productos
+        [HttpGet("getPedidosProductos")]
+        public ActionResult GetPedidosProductos()
+        {
+            var con = from mov in _context.Set<MovimientoItem>()
+                      from cli in _context.Set<Cliente>()
+                      from ped in _context.Set<PedidoDeCliente>()
+                      where mov.TipoDocumento == 7
+                            && mov.Estado != "Liquidado"
+                            && mov.Faltantes < mov.Cantidad
+                            && cli.Idcliente == mov.Tercero
+                            && ped.Consecutivo == mov.Consecutivo
+                      group mov by new
+                      {
+                          Id_Producto = mov.CodigoArticulo,
+                          Producto = mov.NombreArticulo,
+                      } into mov
+                      select new
+                      {
+                          mov.Key.Id_Producto,
+                          mov.Key.Producto,
+                          Costo = (mov.Sum(x => x.Cantidad * x.PrecioUnidad)),
+                          Cantidad = mov.Count()
+                      };
+            return Ok(con);
+        }
+
+        //GET: Consulta para obtener el listado de productos
+        [HttpGet("getPedidosVendedores")]
+        public ActionResult GetPedidosVendedores()
+        {
+            var con = from mov in _context.Set<MovimientoItem>()
+                      from cli in _context.Set<Cliente>()
+                      from ped in _context.Set<PedidoDeCliente>()
+                      where mov.TipoDocumento == 7
+                            && mov.Estado != "Liquidado"
+                            && mov.Faltantes < mov.Cantidad
+                            && cli.Idcliente == mov.Tercero
+                            && ped.Consecutivo == mov.Consecutivo
+                      group mov by new
+                      {
+                          Id_Vendedor = mov.Vendedor,
+                          Vendedor = mov.NombreVendedor,
+                      } into mov
+                      select new
+                      {
+                          mov.Key.Id_Vendedor,
+                          mov.Key.Vendedor,
+                          Costo = (mov.Sum(x => x.Cantidad * x.PrecioUnidad)),
+                          Cantidad = mov.Count()
+                      };
+            return Ok(con);
+        }
+
+        //GET: Consulta para obtener el listado de productos
+        [HttpGet("getPedidosEstados")]
+        public ActionResult GetPedidosEstados()
+        {
+            var con = from mov in _context.Set<MovimientoItem>()
+                      from cli in _context.Set<Cliente>()
+                      from ped in _context.Set<PedidoDeCliente>()
+                      where mov.TipoDocumento == 7
+                            && mov.Estado != "Liquidado"
+                            && mov.Faltantes < mov.Cantidad
+                            && cli.Idcliente == mov.Tercero
+                            && ped.Consecutivo == mov.Consecutivo
+                      group mov by new
+                      {
+                          mov.Estado,
+                      } into mov
+                      select new
+                      {
+                          mov.Key.Estado,
+                          Costo = (mov.Sum(x => x.Cantidad * x.PrecioUnidad)),
+                          Cantidad = mov.Count()
+                      };
+            return Ok(con);
+        }
+
+        //GET: Consulta para obtener el listado de productos
+        [HttpGet("getPedidosStock")]
+        public ActionResult GetPedidosStock()
+        {
+            var con = from mov in _context.Set<MovimientoItem>()
+                      from cli in _context.Set<Cliente>()
+                      from ped in _context.Set<PedidoDeCliente>()
+                      where mov.TipoDocumento == 7
+                            && mov.Estado != "Liquidado"
+                            && mov.Faltantes < mov.Cantidad
+                            && cli.Idcliente == mov.Tercero
+                            && ped.Consecutivo == mov.Consecutivo
+                            && (from art in _context.Set<Articulo>()
+                                join ext in _context.Set<Existencia>() on art.IdArticulo equals ext.Articulo
+                                where art.Codigo == mov.CodigoArticulo
+                                    && art.DesHabilitado == false
+                                    && art.Presentacion == mov.Presentacion
+                                group ext by ext.Articulo into ext
+                                select ext.Sum(x => x.Existencias)).FirstOrDefault() >= (mov.Cantidad - mov.Faltantes)
+                      select new
+                      {
+                          mov.Consecutivo,
+                          Fecha_Creacion = mov.FechaDocumento,
+                          Id_Cliente = mov.Tercero,
+                          Cliente = mov.NombreTercero,
+                          cli.Ciudad,
+                          Id_Producto = mov.CodigoArticulo,
+                          Producto = mov.NombreArticulo,
+                          Existencias = (from art in _context.Set<Articulo>()
+                                         join ext in _context.Set<Existencia>() on art.IdArticulo equals ext.Articulo
+                                         where art.Codigo == mov.CodigoArticulo
+                                               && art.DesHabilitado == false
+                                               && art.Presentacion == mov.Presentacion
+                                         group ext by ext.Articulo into ext
+                                         select ext.Sum(x => x.Existencias)).FirstOrDefault(),
+                          Cant_Pedida = mov.Cantidad,
+                          Cant_Pendiente = (mov.Cantidad - mov.Faltantes),
+                          Cant_Facturada = mov.Faltantes,
+                          mov.Presentacion,
+                          mov.PrecioUnidad,
+                          Id_Vendedor = mov.Vendedor,
+                          Vendedor = mov.NombreVendedor,
+                          Orden_Compra_CLiente = ped.OrdenCompraCliente,
+                          Costo_Cant_Pendiente = ((mov.Cantidad - mov.Faltantes) * mov.PrecioUnidad),
+                          Costo_Cant_Total = (mov.Cantidad * mov.PrecioUnidad),
+                          Fecha_Entrega = ped.FechaEntrega,
+                          mov.Estado,
                       };
             return Ok(con);
         }
