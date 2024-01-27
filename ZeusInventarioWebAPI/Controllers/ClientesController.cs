8926ca12798dc994a83abefde698a905f1af58d7
Diego Fernandez <diegofe21605@gmail.com>
2024-01-27 10:42:53 -0500

    Creación de Controlador para Clientes
    Se creó un controlador para la tabla Clientes.

---
+++ ZeusInventarioWebAPI/Controllers/ClientesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZeusInventarioWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController, Authorize]
+    public class ClientesController : ControllerBase
+    {
+        private readonly Data.InventarioDataContext _context;
+
+        public ClientesController(Data.InventarioDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("getClientByName/{name}")]
+        public ActionResult getClientBtName(string name)
+        {
+            var clients = from c in _context.Set<Cliente>()
+                          where c.Razoncial.Contains(name)
+                          select c;
+            return clients.Any() ? Ok(clients) : NotFound();
+        }
+
+        [HttpGet("getClientById/{id}")]
+        public ActionResult getClientById(string id)
+        {
+            var client = from c in _context.Set<Cliente>()
+                         where c.Idtercero == id
+                         select c;
+            return client.Any() ? Ok(client) : NotFound();
+        }
+
+        [HttpGet("getClientByIdThird/{third}")]
+        public ActionResult getClientBythird(string third)
+        {
+            var client = from c in _context.Set<Cliente>()
+                         where c.Idcliente == third
+                         select c;
+            return client.Any() ? Ok(client) : NotFound();
+        }
+    }
+}
--- ZeusInventarioWebAPI/obj/Release/net8.0/ZeusInventarioWebAPI.AssemblyInfo.cs
+++ ZeusInventarioWebAPI/obj/Release/net8.0/ZeusInventarioWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZeusInventarioWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24eb01c6d62cf7adec2a26c8ea3a6109096f924f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a2dfd07bcdf595dce10737d11c34e050dfcf83d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZeusInventarioWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZeusInventarioWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
